e21bb62c6f2cbc2ea2bc80ca7524296d69a4d978
Adhiambo_C <cinadhiambo22@gmail.com>
2020-04-20 00:03:39 +0300

    GSAP Timelines

---
+++ animating-vue-lesson2-START/src/views/Timeline.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <img class="runner first" src="../assets/runner.png" alt="runner" />
+        <img class="runner second" src="../assets/runner.png" alt="runner" />
+         <img class="runner third" src="../assets/runner.png" alt="runner" />
+    </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+export default {
+    mounted() {
+    // timeline will go here
+    let tl = gsap.timeline()
+    tl.to('.first', { x: 700, duration: 2, ease: 'expo.out' })
+    tl.to('.second', { x: 700, duration: 2, ease: 'expo.out' }, 0.5) // now with an absolute position animation to fire a half second into the timeline
+    tl.to('.third', { x: 700, duration: 2, ease: 'expo.out' })
+    //To set a relative position, we use a string like '-=.75' or +=.75 to set the start of one tween .75 seconds before (-=) or after (+=) the end of the animation before it
+  }
+    }
+</script>
+
+<style lang="scss" scoped>
+.runner {
+  display: block;
+  height: 5em;
+  width: 5em;
+  margin-top: 1.5em;
+}
+</style>
